44fdeb77f91a7905c585742c927772a7cf6736cd
VINICIUS DA LUZ LIMA CABRAL PINTO <202402159216@alunos.ibmec.edu.br>
2024-10-03 17:03:14 -0300

    node.js

---
+++ src/components/NovoComponente/NovoComponente.jsx
@@ -0,0 +1,9 @@
+const NovoComponente = ( ) => (
+    <div>
+        <h2>Conteúdo do Componente</h2> 
+        <p>Um parágrafo</p>
+    </div>
+
+);
+
+export default NovoComponente;
---
+++ src/pages/Nova.jsx
@@ -0,0 +1,11 @@
+import NovoComponente from "../components/NovoComponente/NovoComponente";
+
+const Nova = () => {
+    return(<>
+                <p>oiiiiiiii</p>
+                <h1>Pagina da Aula 03/10</h1>
+                <NovoComponente/>
+        </>);
+} 
+
+export default Nova;
